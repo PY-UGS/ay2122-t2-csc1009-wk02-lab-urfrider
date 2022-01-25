1fc9a6fe621ff1769796f0237d97bda50c91ad8e
urfrider <98369910+urfrider@users.noreply.github.com>
2022-01-25 12:38:21 +0800

    Add files via upload

---
+++ Question1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Question1 {
+
+	final static double PI = 3.14159;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a number for radius: ");
+		float radius = sc.nextFloat();
+		
+		System.out.print("The area for the circle of radius " + radius + " is " + (radius*radius*PI));
+		sc.close();
+
+	}
+
+}
